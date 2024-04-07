3d4ef0c46f6f43334d512bcd6d48ce13db23b575
Jafetty95 <158544210+Jafetty95@users.noreply.github.com>
2024-04-07 12:48:35 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-1_gw67COpIK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3n2h68dttse4juekehlos15n5n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
